aa4a7a1b749196cb182a0d7273d8cc93d7814025
devtencodes <devtencodes@gmail.com>
2022-03-17 22:47:10 +0100

    Finish Ex. 7

---
+++ Lab1Maven/src/main/java/Main.java
@@ -0,0 +1,24 @@
+import java.text.SimpleDateFormat;
+import java.util.*;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class Main {
+
+    public static void main(String[] args) throws JsonProcessingException {
+
+
+
+        // Converting object to json
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        User userObject = new User("John", 21);
+        String userJson = objectMapper.writeValueAsString(userObject);
+
+        System.out.println(userJson); // {"name":"John","age":21}
+        // ----------------------
+    }
+
+
+}
