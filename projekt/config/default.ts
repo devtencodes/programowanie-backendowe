77181b3797c52c893ce0cafdc244b85a6947b398
piotr.tendera <piotr.tendera@prisjakt.nu>
2022-07-06 12:53:21 +0200

    Add password encryption to usermodel

--- projekt/config/default.ts
+++ projekt/config/default.ts
@@ -1,4 +1,5 @@
 export default {
     port: 1337,
     dbUri: "mongodb://localhost:27017/mongo_database",
+    saltWorkFactor: 10,
 };
--- projekt/src/models/user.model.ts
+++ projekt/src/models/user.model.ts
@@ -8,6 +8,7 @@ export interface UserDocument extends mongoose.Document {
     password: string;
     createdAt: Date;
     updatedAt: Date;
+    comparePassword(candidatePassword: string): Promise<Boolean>;
 }
 
 const userSchema = new mongoose.Schema(
@@ -20,6 +21,29 @@ const userSchema = new mongoose.Schema(
         timestamps: true,
     }
 );
+
+userSchema.pre("save", async function (next) {
+    let user = this as UserDocument;
+
+    if (!user.isModified("password")) {
+        return next();
+    }
+
+    const salt = await bcrypt.genSalt(config.get<number>("saltWorkFactor"));
+    const hash = await bcrypt.hashSync(user.password, salt);
+
+    user.password = hash;
+    return next();
+});
+
+userSchema.methods.comparePassword = async function (
+    candidatePassword: string
+): Promise<boolean> {
+    const user = this as UserDocument;
+
+    // if candidate password is correct it's going to return true
+    return bcrypt.compare(candidatePassword, user.password).catch((e) => false);
+};
 
 const UserModel = mongoose.model("User", userSchema);
 
