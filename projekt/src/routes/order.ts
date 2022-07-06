ef199c02a61678a478a254d54feadacb54cbebb5
piotr.tendera <piotr.tendera@prisjakt.nu>
2022-07-06 11:21:19 +0200

    initial commit - installing dependencies

--- projekt/src/routes/order.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import * as orderController from "../controllers/order";
-const router = Router();
-
-router.post("/:orderId", orderController.getOrder);
-router.post("/", orderController.getOrders);
-
-export default router;
