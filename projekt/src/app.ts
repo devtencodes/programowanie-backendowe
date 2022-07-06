7988e1840133c84562a45060018228f017826a6f
piotr.tendera <piotr.tendera@prisjakt.nu>
2022-07-06 12:21:39 +0200

    Add first schema validation

--- projekt/src/app.ts
+++ projekt/src/app.ts
@@ -4,7 +4,7 @@ import connect from "./utils/connect";
 import logger from "./utils/logger";
 import routes from "./routes";
 
-const port = config.get<number>("port");
+const port = config.get<number>("port") ?? "";
 
 const app = express();
 
---
+++ projekt/src/middleware/validateResource.ts
@@ -0,0 +1,19 @@
+import { Request, Response, NextFunction } from "express";
+import { AnyZodObject } from "zod";
+
+// Currying, it is going to validate this request object against that schema
+const validate =
+    (schema: AnyZodObject) =>
+    (req: Request, res: Response, next: NextFunction) => {
+        try {
+            schema.parse({
+                body: req.body,
+                query: req.query,
+                params: req.params,
+            });
+        } catch (e: any) {
+            return res.status(400).send(e.errors);
+        }
+    };
+
+export default validate;
