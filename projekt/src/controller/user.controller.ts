03151417e8678395cb8a9cc19d6b32dd0e809c76
piotr.tendera <piotr.tendera@prisjakt.nu>
2022-07-06 15:12:40 +0200

    Add omiting password for user response

--- projekt/src/controller/user.controller.ts
+++ projekt/src/controller/user.controller.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
 import { createUser } from "../service/user.service";
+import { omit } from "lodash";
 import { CreateUserInput } from "../schema/user.schema";
 import logger from "../utils/logger";
 
@@ -9,7 +10,7 @@ export async function createUserHandler(
 ) {
     try {
         const user = await createUser(req.body);
-        return res.send(user);
+        return res.send(omit(user.toJSON(), "password"));
     } catch (e: any) {
         logger.error(e);
         return res.status(409).send(e.message);
