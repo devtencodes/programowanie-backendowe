b74f1896fa7a7bca8b36215c9835ab9848abb070
piotr.tendera <piotr.tendera@prisjakt.nu>
2022-07-06 12:00:41 +0200

    Add pino-pretty logging

--- projekt/src/utils/logger.ts
+++ projekt/src/utils/logger.ts
@@ -1,11 +1,20 @@
 import logger from "pino";
 import dayjs from "dayjs";
+import pretty from "pino-pretty";
 
-const log = logger({
-    base: {
-        pid: false, // pid stands for process id
-    },
-    timestamp: () => `,"time":"${dayjs().format()}"`,
+// Needed for formatting logging output
+const stream = pretty({
+    colorize: true,
 });
+
+const log = logger(
+    {
+        base: {
+            pid: false, // pid stands for process id
+        },
+        timestamp: () => `,"time":"${dayjs().format()}"`,
+    },
+    stream
+);
 
 export default log;
