771356d0282e9884af6d221b837e8d7b4ebeb6ae
piotr.tendera <piotr.tendera@prisjakt.nu>
2022-07-09 00:26:22 +0200

    Add session schema

--- projekt/src/routes.ts
+++ projekt/src/routes.ts
@@ -1,7 +1,9 @@
 import { Express, Request, Response } from "express";
 import { createUserHandler } from "./controller/user.controller";
+import { createUserSessionHandler } from "./controller/session.controller";
 import validateResource from "./middleware/validateResource";
 import { createUserSchema } from "./schema/user.schema";
+import { createSessionSchema } from "./schema/session.schema";
 
 function routes(app: Express) {
     app.get("/healthcheck", (req: Request, res: Response) =>
@@ -12,6 +14,12 @@ function routes(app: Express) {
         "/api/users",
         validateResource(createUserSchema),
         createUserHandler
+    );
+
+    app.post(
+        "/api/sessions",
+        validateResource(createSessionSchema),
+        createUserSessionHandler
     );
 }
 export default routes;
