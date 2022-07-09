87c74e9b5e25aeb8a003fcf5809db520187b4de1
piotr.tendera <piotr.tendera@prisjakt.nu>
2022-07-09 19:37:14 +0200

    Add product routes

--- projekt/src/routes.ts
+++ projekt/src/routes.ts
@@ -41,5 +41,29 @@ function routes(app: Express) {
 
     app.get("/api/sessions", requireUser, getUserSessionsHandler);
     app.delete("/api/sessions", requireUser, deleteSessionHandler);
+
+    app.post(
+        "/api/products",
+        [requireUser, validateResource(createProductSchema)],
+        createProductHandler
+    );
+
+    app.put(
+        "/api/products",
+        [requireUser, validateResource(updateProductSchema)],
+        updateProductHandler
+    );
+
+    app.get(
+        "/api/products",
+        validateResource(getProductSchema),
+        getProductHandler
+    );
+
+    app.delete(
+        "/api/products",
+        [requireUser, validateResource(deleteProductSchema)],
+        deleteProductHandler
+    );
 }
 export default routes;
