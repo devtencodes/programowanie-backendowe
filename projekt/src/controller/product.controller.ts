6e713287551353346ccb42dbad6a2a75dcdf9291
piotr.tendera <piotr.tendera@prisjakt.nu>
2022-07-09 18:43:57 +0200

    Add crud handlers to product controllers

--- projekt/src/controller/product.controller.ts
+++ projekt/src/controller/product.controller.ts
@@ -1,7 +1,86 @@
+import {
+    CreateProductInput,
+    UpdateProductInput,
+} from "./../schema/product.schema";
 import { Request, Response, NextFunction } from "express";
+import {
+    createProduct,
+    findAndUpdateProduct,
+    findProduct,
+    deleteProduct,
+} from "../service/product.service";
 
-export async function createProductHandler(req: Request, res: Response) {}
+/*  it refers to zod's schema object properties
+no params, no response body and then just body is needed */
+export async function createProductHandler(
+    req: Request<{}, {}, CreateProductInput["body"]>,
+    res: Response
+) {
+    const userId = res.locals.user._id;
+    const body = req.body;
+    const product = await createProduct({ ...body, user: userId });
 
-export async function updateProductHandler(req: Request, res: Response) {}
-export async function getProductHandler(req: Request, res: Response) {}
-export async function deleteProductHandler(req: Request, res: Response) {}
+    return res.send(product);
+}
+
+export async function updateProductHandler(
+    req: Request<UpdateProductInput["params"]>,
+    res: Response
+) {
+    const userId = res.locals.user._id;
+
+    const productId = req.params.productId;
+    const update = req.body;
+    const product = await findProduct({ productId });
+
+    if (!product) {
+        return res.sendStatus(404);
+    }
+
+    if (product.user !== userId) {
+        return res.sendStatus(403);
+    }
+
+    const updatedProduct = await findAndUpdateProduct({ productId }, update, {
+        new: true,
+    });
+
+    return res.send(updatedProduct);
+}
+
+export async function getProductHandler(
+    req: Request<UpdateProductInput["params"]>,
+    res: Response
+) {
+    const productId = req.params.productId;
+    const product = await findProduct({ productId });
+
+    if (!product) {
+        return res.sendStatus(404);
+    }
+
+    return res.send(product);
+}
+
+export async function deleteProductHandler(
+    req: Request<UpdateProductInput["params"]>,
+    res: Response
+) {
+    const userId = res.locals.user._id;
+
+    const productId = req.params.productId;
+
+    const product = await findProduct({ productId });
+
+    if (!product) {
+        return res.sendStatus(404);
+    }
+
+    if (product.user !== userId) {
+        return res.sendStatus(403);
+    }
+
+    await deleteProduct({ productId });
+
+    return res.sendStatus(200);
+}
