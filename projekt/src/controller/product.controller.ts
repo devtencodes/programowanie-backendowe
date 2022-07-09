c67f86791c024da85a9011abc04431afd94b1960
piotr.tendera <piotr.tendera@prisjakt.nu>
2022-07-09 18:20:57 +0200

    Add first-schema validation for product and add basic product controller

---
+++ projekt/src/controller/product.controller.ts
@@ -0,0 +1,7 @@
+import { Request, Response, NextFunction } from "express";
+
+export async function createProductHandler(req: Request, res: Response) {}
+
+export async function updateProductHandler(req: Request, res: Response) {}
+export async function getProductHandler(req: Request, res: Response) {}
+export async function deleteProductHandler(req: Request, res: Response) {}
